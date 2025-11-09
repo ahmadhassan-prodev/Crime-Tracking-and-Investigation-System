de0465266db89f50839f62246e8368b7f53a48c1
Ahmad Hassan <ahmadhassan.prodev@gmail.com>
2025-11-09 15:07:16 +0500

    Initialize classes

---
+++ Evidence.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Evidence {
+public:
+    int id;
+    string type;
+    string description;
+    string dateCollected;
+
+    Evidence(int i, string t, string dp, string da){
+        id = i;
+        type = t;
+        description = dp;
+        dateCollected = da;
+    }
+
+    void display(){
+        cout << "ID: " << id
+             << " | Type: " << type
+             << " | Description: " << description
+             << " | Date: " << dateCollected << endl;
+    }
+};
